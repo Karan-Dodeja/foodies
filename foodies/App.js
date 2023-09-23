d793b40e5e6183a91e697b21e57b66611d5c2fd0
perceptkraft <141613035+perceptkraft@users.noreply.github.com>
2023-09-23 17:41:43 +0530

    add yarn in project  and eslint setup

--- foodies/App.js
+++ foodies/App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Text>search</Text>
+        </View>
+        <View stye={styles.list}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
+  
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+
+  search : {
+    padding: 16,
+    backgroundColor: "green"
+  },
+
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue"
+  }
+
 });
